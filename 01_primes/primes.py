98e899a145dd8cece76402c15b9ec05ba85764f2
Guillaume Ricard <guillaume.ricard@mail.com>
2017-09-09 18:20:11 +0200

    primes: added project
    Added README.md and first Python project: `primes`

---
+++ 01_primes/primes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from math import sqrt
+
+
+def is_prime(n, primes=None):
+    if primes is not None:
+        for p in primes:
+            if n % p == 0:
+                return False
+            elif p > sqrt(n):
+                return True
+    else:
+        for p in range(2, n):
+            if n % p == 0:
+                return False
+            elif n > sqrt(n):
+                return True
+
+
+def next_prime(n, primes=None):
+    m = n + 1
+    while not is_prime(m, primes):
+        m = m + 1
+    return m
+
+
+if __name__ == "__main__":
+    primes = [2]
+    while len(primes) < 100:
+        primes.append(next_prime(primes[-1]))
+    print(primes)
---
+++ README.md
@@ -0,0 +1,11 @@
+MPSI Lycée Jean-Baptiste Corot
+==============================
+
+This GitHub repository is meant to keep track of various projects that I'll be
+designing this year during my IT classes.
+
+Feel free to contribute, comment, fork or whatever!
+
+----------------
+
+&copy; 2017 Guillaume Ricard - License MIT
